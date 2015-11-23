1ed33735f0495bd2c566aa97b20f0181d9d11bde
Greg Berenfield <gberenfield@berenfield.com>
2015-11-23 09:43:46 -0500

    my fixes for powerline

--- segments/tmux_session_info.sh
+++ segments/tmux_session_info.sh
@@ -1,7 +1,12 @@
 # Prints tmux session info.
 # Assuems that [ -n "$TMUX"].
 
+left_separator_black=''
+tmux_conf_theme_session_fg=${tmux_conf_theme_session_fg:-colour16}  # black
+tmux_conf_theme_session_bg=${tmux_conf_theme_session_bg:-colour226} # yellow
+status_left="❐ #S:#I/#{session_windows}"
 run_segment() {
-	tmux display-message -p '#S:#I.#P'
-	return 0
+tmux display-message -p "$status_left"
+return 0
 }
+# tmux display-message -p '#S:#I.#P'
